03055606fe284aaad71cc0e593ed20dfd89ea195
endrasakya076-pixel <endrasakya076@gmail.com>
2025-09-05 13:24:29 +0800

    coommit hendra

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document Hendra Sakya</title>
+    <title>Document Hendra Sakya Pasti Bisa</title>
 </head>
 <body>
     
